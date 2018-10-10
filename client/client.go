4ba0c6c2d0fa01bfe249016d5e770b11dd6fdd76
Trevor Forrey <trevor4e@gmail.com>
2018-10-10 15:16:55 -0700

    client/server with get & set ops on in memory rb tree

--- client/client.go
+++ client/client.go
@@ -11,22 +11,24 @@ func main() {
 	if err != nil {
 		fmt.Println("Error connecting to server")
 	}
-	request := "Get key1\r\n"
+
+	fmt.Println("About to send SET request")
+	request := "SET key1 bananas\r\n"
 	fmt.Fprint(conn, request)
 
 	message, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
 		fmt.Println("Error getting response back from server")
 	}
-	fmt.Printf("Server Response: %s", message)
+	fmt.Printf("Server Response: %s\n", message)
 
 	conn, err = net.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
 		fmt.Println("Error connecting to server")
 	}
 
-	request = "Get key2\r\n"
-	fmt.Println("About to send a request")
+	request = "GET key1\r\n"
+	fmt.Println("About to send GET request")
 	fmt.Fprint(conn, request)
 	fmt.Println("Request sent")
 
--- server/server.go
+++ server/server.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"OttoDB/server/rbTree"
 	"bufio"
+	"errors"
 	"fmt"
 	"net"
 	"strings"
+)
+
+type operation struct {
+	op    string
+	key   string
+	value string
+}
+
+var (
+	tree     = rbTree.NewTree()
+	validOps = map[string]struct{}{"GET": {}, "SET": {}, "DEL": {}, "QUIT": {}, "BEGIN": {}, "COMMIT": {}}
 )
 
 func main() {
@@ -15,23 +28,61 @@ func main() {
 	defer ln.Close()
 	for {
 		conn, err := ln.Accept()
-		fmt.Printf("Recieved a request")
+		fmt.Println("Recieved a request")
 		if err != nil {
 			fmt.Printf("Error while accepting connection %s", err)
 		}
-		go handleRequest(conn)
+		go handleConnection(conn)
 	}
 }
 
-func handleRequest(conn net.Conn) {
+func handleConnection(conn net.Conn) {
 	fmt.Println("About to handle request")
+	defer conn.Close()
 	fullOp, err := bufio.NewReader(conn).ReadString('\n')
+	operation, err := parseOp(fullOp)
 	if err != nil {
-		fmt.Printf("Error reading message from client\n")
+		fmt.Printf("Error reading message from client: %s\n", err)
 	}
-	fmt.Printf("Received: %s", fullOp)
-	fmt.Fprint(conn, "Request received\n")
-	ops := strings.Fields(fullOp)
-	fmt.Println(ops)
-	conn.Close()
+	response, err := performOp(operation)
+	if err != nil {
+		fmt.Printf("Error performing operation %s\n", err)
+	}
+	fmt.Println("About to send response")
+	fmt.Fprint(conn, response)
+}
+
+func parseOp(fullOp string) (operation, error) {
+	splitOps := strings.Fields(fullOp)
+	var newOp operation
+	newOp.op = splitOps[0]
+	newOp.key = splitOps[1]
+	if newOp.op == "SET" {
+		newOp.value = splitOps[2]
+	}
+	fmt.Printf("Received %s operaiton\n", newOp.op)
+
+	// Check that new op is a valid op, if not, return error
+	_, valid := validOps[newOp.op]
+	if !valid {
+		return newOp, errors.New("Invalid operation requested: ")
+	}
+	return newOp, nil
+}
+
+func performOp(op operation) (string, error) {
+	if op.op == "GET" {
+		fmt.Println("About to perform get request")
+		keyVal := tree.Get(op.key)
+		fmt.Printf("Retrieved %s from tree\n", keyVal)
+		return keyVal, nil
+	} else if op.op == "SET" {
+		fmt.Println("About to perform set request")
+		fmt.Printf("Key: %s\n", op.key)
+		fmt.Printf("Value: %s\n", op.value)
+		tree.Set(op.key, op.value)
+		tree.InOrderTraversal()
+		return "set value in db", nil
+	}
+	return "operation didn't match", errors.New("Op didn't match")
 }
